27e34f31a441e1556a9ffbd7affe763d64f1af23
Gabriela <gabrielailarreta@gmail.com>
2018-08-07 09:37:11 -0400

    Ejercicio1

---
+++ ejercicio1.rb
@@ -0,0 +1,25 @@
+class Alumno
+	alumnos
+  def initialize(nombre, nota1, nota2, nota3, nota4)
+    @nombre = nombre
+    @nota1 = nota1
+    @nota2 = nota2
+    @nota3 = nota3
+    @nota4 = nota4
+  end
+
+  def read_file(archivo = 'notas.txt')
+	alumnos = []
+	data = []
+	File.open(archivo, 'r') { |file| data = file.readlines }
+	data.each do |alumno|
+	alumnos << Alumno.new(*alumno.split(', '))
+	end
+	print alumnos
+  end
+end
+
+
+a = Alumno.new.read_file()
+
+puts a
